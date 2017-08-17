beffadafbb5ce96bb4529fa6cc2c54c557f73700
Adam Coard <acoard@advantcomp.com>
2017-08-17 08:39:50 -0700

    Refactored modal, reducing files and simplifying personal info page.

--- src/app/core/consent-modal/consent-modal.component.ts
+++ src/app/core/consent-modal/consent-modal.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 import { BaseComponent } from '../../core/base-component/base-component.component'
-import { ConsentModalContent } from './content/consent-modal-content.component';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+// import { ConsentModalContent } from './content/consent-modal-content.component';
 
 @Component({
   selector: 'app-consent-modal',
   templateUrl: './consent-modal.component.html',
   styleUrls: ['./consent-modal.component.scss']
 })
-export class ConsentModalComponent extends BaseComponent implements OnInit {
-  bsModalRef: BsModalRef;
+export class ConsentModalComponent extends BaseComponent implements AfterViewInit {
+  bsModalRef: BsModalRef
+
+  // @ViewChild('informationCollectionModal') public informationCollectionModal: ModalDirective;
+  @ViewChild('informationCollectionModal') public informationCollectionModal:ModalDirective;
+
 
   constructor(private modalService: BsModalService) {
     super();
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.openModal();
   }
 
-  openModal() {
-    console.log('openModal called');
-    this.bsModalRef = this.modalService.show(ConsentModalContent);
+  public openModal(): void {
+    this.informationCollectionModal.show();
   }
+
+  public closeModal(): void {
+    this.informationCollectionModal.hide();
+  }
+
+
 
 }
