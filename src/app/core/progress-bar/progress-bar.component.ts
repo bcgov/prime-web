7ffa1fce1db44a3e411b2d5bf9e956144da772b3
Adam Coard <acoard@advantcomp.com>
2017-08-17 09:35:55 -0700

    Improved progres bar.

--- src/app/core/progress-bar/progress-bar.component.ts
+++ src/app/core/progress-bar/progress-bar.component.ts
@@ -10,17 +10,37 @@ import { ProgressBarItem } from './progress-bar.interface';
 })
 export class ProgressBarComponent implements OnInit {
   progressBarList: ProgressBarItem[] = [
-    {displayName: 'Personal Information', routerLink: 'professional-info'},
-    {displayName: 'Site Access', routerLink: 'site-access'},
-    {displayName: 'Contact Information', routerLink: 'contact-info'},
-    {displayName: 'Self Declaration', routerLink: 'self-declaration'},
-    {displayName: 'User Acceptance', routerLink: 'user-acceptance'},
-    {displayName: 'Review & Submit ', routerLink: 'review'},
+    {displayName: 'Personal Information', routerLink: '/professional-info'},
+    {displayName: 'Site Access', routerLink:  '/site-access'},
+    {displayName: 'Contact Information', routerLink: '/contact-info'},
+    {displayName: 'Self Declaration', routerLink: '/self-declaration'},
+    {displayName: 'User Acceptance', routerLink: '/user-acceptance'},
+    {displayName: 'Review & Submit ', routerLink: '/review'},
   ]
 
   constructor(public router: Router) { }
 
   ngOnInit() {
   }
+
+  public isActive(item: ProgressBarItem): boolean {
+    return this.router.isActive(item.routerLink, false);
+  }
+
+  public isAfterActive(item: ProgressBarItem): boolean {
+    let routeIndex = -1;
+    let activeRouteIndex = -1;
+    for (let i = 0; i < this.progressBarList.length; i++) {
+      if (this.progressBarList[i].routerLink === item.routerLink) {
+        routeIndex = i;
+      }
+      if (this.isActive(this.progressBarList[i])) {
+        activeRouteIndex = i;
+      }
+    }
+    return routeIndex > activeRouteIndex;
+  }
+
+
 
 }
