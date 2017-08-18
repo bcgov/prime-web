a8c649c4dcd56c8e8917cc4afd7cf5817993485a
Adam Coard <acoard@advantcomp.com>
2017-08-18 09:48:25 -0700

    Implemented select2-bootstrap-theme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { ConsentModalComponent } from './core/consent-modal/consent-modal.compon
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(){
+
+    //Set app-wide configuration for select2.
+    (<any>$.fn.select2).defaults.set( "theme", "bootstrap" );
+  }
 }
