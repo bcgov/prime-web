8be67ea6cf77153b74b4044515125a3ec5586701
Adam Coard <acoard@advantcomp.com>
2017-08-23 12:59:44 -0700

    Refactored applicant to have a goesToCollege method, and used it to properly implement form hiding on professional info page.

--- src/app/models/applicant.ts
+++ src/app/models/applicant.ts
@@ -31,4 +31,18 @@ export class Applicant {
   get collegeNumber(): string{
     return this.college ? this.college.toString() : '';
   }
+
+  /**
+   * Determines if applicant goes to college. Can be left blank to see if they go to any college. To check if option is "none", must explicitly pass Colleges.None
+   * @param college optional, should be Colleges or undefined.
+   */
+  goesToCollege(college? : Colleges): boolean {
+    if (!this.college) return false;
+    if (this.college.length  ===  0 && this.college[0] == Colleges.None) {
+      return this.college[0] === college;
+    }
+    if (this.college === Colleges.None) return this.college === college;
+    if (!college && this.college.length) return true;
+    return (<Colleges[]>this.college).indexOf(college) >= 0;
+  }
 }
