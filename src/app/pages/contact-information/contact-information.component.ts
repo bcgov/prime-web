d331d0f139b2a206530cb7f7419ac3608b24634e
Adam Coard <acoard@advantcomp.com>
2017-08-25 10:34:43 -0700

    Tweaking file upload to properly match the form.

--- src/app/pages/contact-information/contact-information.component.ts
+++ src/app/pages/contact-information/contact-information.component.ts
@@ -13,7 +13,8 @@ export class ContactInformationComponent implements OnInit {
 
   public countries;
   public applicant: Applicant;
-  public securityQuestions: {question: string, selected: boolean}[];
+  // public securityQuestions: {question: string, selected: boolean}[];
+  public securityQuestions: any[];
 
   constructor(private router: Router,
   private applicantData: ApplicantDataService) {
@@ -31,17 +32,30 @@ export class ContactInformationComponent implements OnInit {
       }
     ]
 
+    // this.securityQuestions = [
+    //   {question: "What was your first pet's name?", selected: true},
+    //   {question: "What was the make of your first car?", selected: true},
+    //   {question: "What was the last name of your favourite teacher?", selected: false},
+    //   {question: "What was the last name of your childhood best friend?", selected: false},
+    //   {question: "What is your oldest cousin's first name?", selected: false},
+    //   {question: "What town was your father born in?", selected: false},
+    //   {question: "What town was your mother born in?", selected: false},
+    //   {question: "Where did you meet your spouse?", selected: false},
+    //   {question: "What year did you meet your spouse?", selected: false},
+    //   {question: "What is the name of your favourite book?", selected: false}
+    // ]
+
     this.securityQuestions = [
-      {question: "What was your first pet's name?", selected: true},
-      {question: "What was the make of your first car?", selected: true},
-      {question: "What was the last name of your favourite teacher?", selected: false},
-      {question: "What was the last name of your childhood best friend?", selected: false},
-      {question: "What is your oldest cousin's first name?", selected: false},
-      {question: "What town was your father born in?", selected: false},
-      {question: "What town was your mother born in?", selected: false},
-      {question: "Where did you meet your spouse?", selected: false},
-      {question: "What year did you meet your spouse?", selected: false},
-      {question: "What is the name of your favourite book?", selected: false}
+      {text: "What was your first pet's name?", id: 1},
+      {text: "What was the make of your first car?", id: 2},
+      {text: "What was the last name of your favourite teacher?", id: 3},
+      {text: "What was the last name of your childhood best friend?", id: 4},
+      {text: "What is your oldest cousin's first name?", id: 5},
+      {text: "What town was your father born in?", id: 6},
+      {text: "What town was your mother born in?", id: 7},
+      {text: "Where did you meet your spouse?", id: 8},
+      {text: "What year did you meet your spouse?", id: 9},
+      {text: "What is the name of your favourite book?", id: 10}
     ]
    }
 
