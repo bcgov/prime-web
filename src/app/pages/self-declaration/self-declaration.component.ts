9958b488966df11e1889265b59455f0783f90fc5
Adam Coard <acoard@advantcomp.com>
2017-08-31 14:57:06 -0700

    Improved scrolling logic via a scaling coefficient.

--- src/app/pages/self-declaration/self-declaration.component.ts
+++ src/app/pages/self-declaration/self-declaration.component.ts
@@ -30,17 +30,24 @@ export class SelfDeclarationComponent extends BaseComponent implements OnInit {
     //TODO - Refactor to get height directly from .upload-section.
     //Until this approach is approved/rejected, keeping it hardcoded.
     let px;
+
+    let coefficient = this.declarationQuestions.nativeElement.offsetHeight / 850
+    if (coefficient > 1) coefficient = 1
+
     if (this.applicant.hasBeenSuspended
       || this.applicant.hasInformationContraventionOrder
       || this.applicant.hasHadLimitsOrConditions
       || this.applicant.hasPharmaNetEverRevoked
       || this.applicant.hasRevocationBeenResolved) {
-        px = this.declarationQuestions.nativeElement.offsetHeight - 383;
+
+
+        px = (this.declarationQuestions.nativeElement.offsetHeight - 383) * coefficient;
+
     }
     else {
       px = 0;
     }
-    console.log('ngAfterViewChecked', this.declarationQuestions.nativeElement.offsetHeight);
+    console.log('ngAfterViewChecked', this.declarationQuestions.nativeElement.offsetHeight, coefficient, px);
 
     // if (px < 100) px = 0;
     this.viewHeight = px;
