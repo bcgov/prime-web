ac3618cce88605289de212bbd2a3291051662ad0
Adam Coard <acoard@advantcomp.com>
2017-09-01 09:24:43 -0700

    Refactoring and documenting site-access component's data.

---
+++ src/app/models/named-collections.interface.ts
@@ -0,0 +1,10 @@
+export interface NamedCollection {
+  id: number,
+  selected: boolean,
+  namedCollection: string,
+  siteName: string,
+  city: string,
+  postal: string,
+  vendor: string,
+  siteType: string,
+}
