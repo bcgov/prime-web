9d73acafd2a4eb94b6a5981ab035912c23eeabff
Adam Coard <acoard@advantcomp.com>
2017-09-06 08:25:29 -0700

    Removed vestigal imports from removed components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from 'app/dashboard/dashboard.component';
-import { CustomerComponent } from 'app/customer/customer.component';
 import { ProfessionalInfoComponent } from './pages/professional-info/professional-info.component';
 import { ConsentModalComponent } from './core/consent-modal/consent-modal.component';
 
