ad3d3a52736dba883453c7855d40316bc2ad8e65
Adam Coard <acoard@advantcomp.com>
2017-09-07 11:36:48 -0700

    Added ElementRef typing to professional info component.

--- src/app/pages/professional-info/professional-info.component.ts
+++ src/app/pages/professional-info/professional-info.component.ts
@@ -17,6 +17,10 @@ import { AdvancedPracticeCerts } from '../../models/advanced-practice-certs.enum
 export class ProfessionalInfoComponent extends BaseComponent implements OnInit {
   public applicant: Applicant;
   @ViewChild(ConsentModalComponent) private consentModal: ConsentModalComponent;
+  @ViewChild('collegeNameContainer') private collegeNameContainer: ElementRef;
+  @ViewChild('advancedPracticeCertContainer') private advancedPracticeCertContainer: ElementRef;
+
+
   public Colleges: typeof Colleges = Colleges;
   public showError: boolean = false;
   public collegeList: CollegeList[];
@@ -66,33 +70,28 @@ export class ProfessionalInfoComponent extends BaseComponent implements OnInit {
     ]
   }
 
-  onAdvancedPracticeCertChanged(value, el){
-    this.applicant.advancedPracticeCerts = this.enforceNoneSelect(value, el);
+  onAdvancedPracticeCertChanged(value: AdvancedPracticeCerts[]): void {
+    this.applicant.advancedPracticeCerts = this.enforceNoneSelect(value, this.advancedPracticeCertContainer);
   }
 
-  onCollegeChange(value: Colleges[], el): void {
-    this.applicant.college = this.enforceNoneSelect(value, el);
+  onCollegeChange(value: Colleges[]): void {
+    this.applicant.college = this.enforceNoneSelect(value, this.collegeNameContainer);
   }
-
 
   ngAfterViewInit() {
-    if (!this.applicant.consentInfoCollection){
+    if (!this.applicant.consentInfoCollection) {
       this.consentModal.openModal();
     }
   }
 
-  onConsented(consent){
+  onConsented(consent: boolean): void {
     this.applicant.consentInfoCollection = consent;
-  }
-
-  collegeDebug() {
-    console.log('collegeDebug');
   }
 
   /**
    * Re-orders the selected elements in the Professional College multiselect box. Now they will be ordered in the same order the user selects them. This step is necessary in order for the code in onCollegeChange() to work, which enforces business logic rules relating to selecting 'None'.
    */
-  initSelect() {
+  initSelect(): void {
     $(this.elementRef.nativeElement).on("select2:select", 'select', function (evt: any) {
       var element = evt.params.data.element;
       var $element = $(element);
@@ -115,16 +114,17 @@ export class ProfessionalInfoComponent extends BaseComponent implements OnInit {
    * detection will not work.
    * @param value The $event.value from <select2>'s (valueChanged)
    * @param el A parent element containing the <select2> element.
+   * @returns {any} Same type[] as value. Potential modifications to array items.
    */
-  enforceNoneSelect(value, el) : any[] {
-    const firstSelected = el.querySelectorAll('.select2-selection__rendered > li')[0].title
+  enforceNoneSelect(value, element: ElementRef): any[] {
+    const firstSelected = element.nativeElement.querySelectorAll('.select2-selection__rendered > li')[0].title
     const isNoneInPreviousSelection = !(firstSelected.toLowerCase() === "none");
     const isNoneInSelection = value.indexOf("none") >= 0;
     const onlyNoneSelected = value.length === 1 && isNoneInPreviousSelection;
-    const otherOptionsSelected = (value.length >= 2 || (value.length === 1 && ! isNoneInSelection))
+    const otherOptionsSelected = (value.length >= 2 || (value.length === 1 && !isNoneInSelection))
 
     //Case: Have items, select 'None' -> clear and show 'None'
-    if (isNoneInSelection && !isNoneInPreviousSelection && otherOptionsSelected){
+    if (isNoneInSelection && !isNoneInPreviousSelection && otherOptionsSelected) {
       return ["none"];
     }
     //Case: Have 'None', select items -> clear and show items.
