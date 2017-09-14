1ec21dd4209903bd26e5d612f9438b9211c92814
Adam Coard <acoard@advantcomp.com>
2017-09-14 14:10:30 -0700

    Significant progress on validation attribute directive.

--- src/app/validation/prime-required.directive.ts
+++ src/app/validation/prime-required.directive.ts
@@ -32,97 +32,104 @@ export class PrimeRequiredDirective {
   private el: ElementRef;
   private input: ElementRef;
   private label: ElementRef;
-  private ERROR_CLASS: string = "has-error";
-
   private view: ViewContainerRef;
   private factoryResolver: ComponentFactoryResolver;
+  /** The CSS class to add to the element with the directive, i.e. form-group */
+  private ERROR_CLASS: string = "has-error";
+  /** A list of all active validation components. Components are created/destroyed when validation fails/passes. */
+  private activeComponents = {}; //todo - typing!
+
+  /** A comma separated list of validation choices. Default: "required" */
+  @Input('primeRequired') validationOptions: string;
 
 
-  /** A list of all active validation components. */
-  private validationComponents = {};
+  private validationComponents = []; //todo - typing!
+
 
   constructor(el: ElementRef, private renderer: Renderer2,
     @Inject(ViewContainerRef) viewContainerRef,
     @Inject(ComponentFactoryResolver) factoryResolver,
     private ref: ChangeDetectorRef) {
-    // console.log('prime required', el);
     this.el = el;
     this.view = viewContainerRef;
     this.factoryResolver = factoryResolver;
   }
 
-  ngOnInit(){
+  ngOnInit() {
     if (!this.check(this.el)) {
       throw new Error("Unable to initialize PrimeRequiredDirective. Directive is on an element without required child elements.");
     }
-
-    // let comp = this.addComponent(RequiredValidationErrorsComponent);
-    // (comp.instance as RequiredValidationErrorsComponent).visible = true;
-    // this.ref.detectChanges();
-    // console.log('added component', comp);
+    this.validationOptions = this.validationOptions || "required";
+    // Load validation components based on validationOptions
+    this.validationOptions.replace(' ', '').split(',').forEach(opt => {
+      if (opt.toLowerCase() === "required") {
+        this.validationComponents.push(RequiredValidationErrorsComponent);
+      }
+      //If you want to add another validation function, add it here.
+    });
   }
 
-  @HostListener('keyup', ['$event']) onKey($event) {
-    // console.log('onKey', $event.type, this.inputVal);
+  @HostListener('keyup')
+  onKey() {
+    //Run validation of EACH of the validation components.
+    this.validationComponents.forEach(this.runValidationComponent.bind(this));
+  }
 
-    //TODO - get option-based loading in of component working,
-    //ie. don't hardcode RequiredValidationErrorsComponent
-
-    if(RequiredValidationErrorsComponent.validate(this.input)){
-      this.setInvalid();
+  /** Runs the logic of a given validation component */
+  private runValidationComponent(validationComponent: ValidationComponent): void{
+    if (!validationComponent.validate(this.input)) {
+      this.setInvalid(validationComponent);
+    }
+    else {
+      this.setValid(validationComponent);
     }
   }
 
-
-  setInvalid(){
-    console.log('SET INVALID CALLED!');
+  setInvalid(validationComponent) {
     this.renderer.addClass(this.el.nativeElement, this.ERROR_CLASS);
-    // let comp = this.prepareComponent(RequiredValidationErrorsComponent);
-    let comp = this.addComponent(RequiredValidationErrorsComponent);
-
-    //TODO - Get fieldName programmatically from label!
-    //TODO - Don't require hardcoding of component instance casting
-      // Backup plan: Have an interface with fieldName?
-
-    // if (comp){
-    //   (comp.instance as ValidationComponent).fieldName = this.labelText;
-    // }
+    let comp = this.addComponent(validationComponent);
   }
 
-  setValid(){
-    this.renderer.removeClass(this.el.nativeElement, this.ERROR_CLASS);
+  setValid(validationComponent) {
+    if (this.activeComponents[validationComponent.name] == null) {
+      return;
+    }
+
+    this.activeComponents[validationComponent.name].destroy();
+    this.activeComponents[validationComponent.name] = null;
+
+    //Only remove class if there are no other active components.
+    const componentsActive = Object.keys(this.activeComponents)
+    .map(key => this.activeComponents[key])
+    .filter(x => x !== null).length >= 1;
+
+    if (!componentsActive){
+      this.renderer.removeClass(this.el.nativeElement, this.ERROR_CLASS);
+    }
   }
 
-  /**
-   * TODO - Move this function to RequiredValidationErrors
-   * Maybe rename RequiredValidationErrors to IsNotEmptyValidationErrors?
-   */
-  // private validateInputIsNotEmpty(): boolean{
-  //   return this.inputVal.length > 0;
-  // }
-
-  private get inputVal(){
+  private get inputVal() {
     return this.input.nativeElement.value;
   }
 
-  private get labelText(){
+  private get labelText() {
     return this.label.nativeElement.textContent;
   }
 
   /** Creates a component and adds it to the view. */
-  private addComponent<T>(componentClass): ComponentRef<T>{
+  private addComponent<T>(componentClass): ComponentRef<T> {
     //Max 1 instance of each component type, to stop duplicate messages.
-    if (this.validationComponents[componentClass.name]){
+    if (this.activeComponents[componentClass.name]) {
       return;
     }
     const component = this.prepareComponent(componentClass);
     this.insertComponent(component);
-    this.validationComponents[componentClass.name] = component;
+    this.activeComponents[componentClass.name] = component;
     return component as ComponentRef<T>;
   }
 
   /** Creates a component but does not add it to the view */
-  private prepareComponent<T>(componentClass): ComponentRef<T>{
+  private prepareComponent<T>(componentClass): ComponentRef<T> {
     const factory = this.factoryResolver.resolveComponentFactory(componentClass)
     const component = factory.create(this.view.parentInjector) as ComponentRef<T>
     (component.instance as ValidationComponent).fieldName = this.labelText;
@@ -130,20 +137,20 @@ export class PrimeRequiredDirective {
   }
 
   /** Inserts an already created component into the view (c.f. prepareComponent()) */
-  private insertComponent(component: ComponentRef<{}>){
+  private insertComponent(component: ComponentRef<{}>) {
     this.view.insert(component.hostView)
   }
 
 
   /** Checks that primeRequired is attached to the right element. It must have exactly one child label and one input label. */
-  private check(el: ElementRef) : boolean{
+  private check(el: ElementRef): boolean {
     // this.input = el;
     this.input = new ElementRef(el.nativeElement.querySelector('input'));
     // this.label = document.querySelector(`[for="${this.input.nativeElement.name}"]`);
     this.label = new ElementRef(document.querySelector(`[for="${this.input.nativeElement.name}"]`));
 
-    if (this.input === null || this.label === null){
-      console.error("PrimeRequiredDirective is on an element without required child elements.", {element: el})
+    if (this.input === null || this.label === null) {
+      console.error("PrimeRequiredDirective is on an element without required child elements.", { element: el })
       return false;
     }
     return true;
--- src/app/validation/required-validation-errors/required-validation-errors.component.ts
+++ src/app/validation/required-validation-errors/required-validation-errors.component.ts
@@ -14,7 +14,6 @@ export class RequiredValidationErrorsComponent implements OnInit {
   }
 
   public static validate(el: ElementRef): boolean {
-    console.log('VALIDATE called!', el.nativeElement.value)
     return el.nativeElement.value.length > 0;
   }
 
