b9a5cec23b6355bac989a4be5e62fa54a4f3597b
Adam Coard <acoard@advantcomp.com>
2017-09-15 15:18:36 -0700

    Made some progress on unit tests, but unfortunately they're failing when they should pass.

--- src/app/validation/phone-validation/phone-validation.component.spec.ts
+++ src/app/validation/phone-validation/phone-validation.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhoneValidationComponent } from './phone-validation.component';
+import { ElementRef } from '@angular/core';
+
 
 describe('PhoneValidationComponent', () => {
   let component: PhoneValidationComponent;
@@ -22,4 +24,20 @@ describe('PhoneValidationComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a regex', () => {
+    expect(PhoneValidationComponent.regex).toBeDefined();
+  });
+
+  it('be able to validate a phone number', () => {
+    expect(PhoneValidationComponent.validate(createWithPhone('123-456-7890'))).toBeTruthy();
+    expect(PhoneValidationComponent.validate(createWithPhone('123-456-7890a'))).toBeTruthy(); //Should fail! but it isn't.
+    debugger;
+    expect(PhoneValidationComponent.validate(createWithPhone('plzfaila'))).toBeTruthy(); //Should fail! but it isn't.
+
+  });
 });
+
+function createWithPhone(phoneNum: string): ElementRef{
+  return new ElementRef(`<input type='text' value='${phoneNum}'>`);
+}
