c86e1bc19d85e08efb200fc1d984eafc17679792
Adam Coard <acoard@advantcomp.com>
2018-05-30 15:39:05 -0700

    Implement prime header
    - Auto-detects what button to make .active based on routes
    - Fixed syntax bug screwing with alignment in Verifier dashboard bar

--- src/app/modules/header-footer/prime-header/prime-header.component.ts
+++ src/app/modules/header-footer/prime-header/prime-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'prime-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimeHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  routeIsActive(url: string): boolean {
+    return this.router.url.includes(url);
+  }
 }
