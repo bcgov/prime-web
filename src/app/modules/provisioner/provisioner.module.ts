0a0390c74381a8f3d4ad48b6b7703343dd8fe382
Adam Coard <acoard@advantcomp.com>
2018-05-30 12:03:22 -0700

    Added prime-enrollment-list to ProvisionerDashboard
    Scaffolding out page for Kyle

--- src/app/modules/provisioner/provisioner.module.ts
+++ src/app/modules/provisioner/provisioner.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ProvisionerDashboardComponent } from './pages/provisioner-dashboard/provisioner-dashboard.component';
 import { CoreModule } from '../core/core.module';
 import { ProvisionerRoutingModule } from './provisioner-routing.modules';
+import { VerifierModule } from '../verifier/verifier.module';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreModule,
-    ProvisionerRoutingModule
+    ProvisionerRoutingModule,
+    VerifierModule,
   ],
-  declarations: [ProvisionerDashboardComponent]
+  declarations: [
+    ProvisionerDashboardComponent
+  ]
 })
 export class ProvisionerModule { }
