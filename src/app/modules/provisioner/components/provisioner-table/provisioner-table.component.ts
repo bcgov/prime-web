17fd162cab0b03a03a72a5ce025e2901fb4d2644
Adam Coard <acoard@advantcomp.com>
2018-05-31 09:10:45 -0700

    Scaffold out Provisioner Details page + passing tests

---
+++ src/app/modules/provisioner/components/provisioner-table/provisioner-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'prime-provisioner-table',
+  templateUrl: './provisioner-table.component.html',
+  styleUrls: ['./provisioner-table.component.scss']
+})
+export class ProvisionerTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/provisioner/pages/provisioner-details/provisioner-details.component.spec.ts
+++ src/app/modules/provisioner/pages/provisioner-details/provisioner-details.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProvisionerDetailsComponent } from './provisioner-details.component';
+import { CoreModule } from '../../../core/core.module';
+import { ProvisionerModule } from '../../provisioner.module';
+import { ProvisionerTableComponent } from '../../components/provisioner-table/provisioner-table.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PrimeDataService } from '../../../../services/prime-data.service';
+import { routes } from '../../provisioner-routing.modules';
+import { ProvisionerDashboardComponent } from '../provisioner-dashboard/provisioner-dashboard.component';
+import { VerifierModule } from '../../../verifier/verifier.module';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DummyDataService } from '../../../../services/dummy-data.service';
 
 describe('ProvisionerDetailsComponent', () => {
   let component: ProvisionerDetailsComponent;
@@ -8,7 +19,16 @@ describe('ProvisionerDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProvisionerDetailsComponent ]
+      declarations: [ ProvisionerDetailsComponent, ProvisionerTableComponent, ProvisionerDashboardComponent,  ],
+      imports: [ CoreModule, RouterTestingModule.withRoutes(routes), VerifierModule ],
+      providers: [ PrimeDataService,
+
+        // Mock the route "data" fields
+        {
+          provide: ActivatedRoute,
+          useValue: { data: Observable.of({type: "user"}) }
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +36,8 @@ describe('ProvisionerDetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProvisionerDetailsComponent);
     component = fixture.componentInstance;
+    const dummyData = new DummyDataService();
+    component.person = dummyData.createPeople(1)[0];
     fixture.detectChanges();
   });
 
