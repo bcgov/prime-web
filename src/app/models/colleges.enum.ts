d3c533109afdffa8c4dda3dee3b1dca0dddb2dd8
denis-klein <denis.klein@maximusbc.ca>
2018-06-01 11:35:42 -0700

    More preliminary changes to the Professional screen

--- src/app/models/colleges.enum.ts
+++ src/app/models/colleges.enum.ts
@@ -1,22 +1,21 @@
 
 /**
  * Professional Information college choices
- * @deprecated - Used in OLD prototype only
  */
-export enum Colleges {
-  None = 'none',
-  /**College of Physicians and Surgeons of BC (CPSBC)*/
-  CPSBC = '91',
+export enum CollegeTypes {
   /**College of Pharmacists Of BC (CPBC)*/
   CPBC = 'P1',
   /**College of Registered Nurses of BC (CRNBC)*/
   CRNBC = '96',
+  /**College of Physicians and Surgeons of BC (CPSBC)*/
+  CPSBC = '91'
 }
-/** @deprecated */
-export interface CollegeList {
-  id: Colleges;
-  text: string;
-}
+
+// /** @deprecated */
+// export interface CollegeList {
+//   id: Colleges;
+//   text: string;
+// }
 
 
 // NEW Colleges Layout... Need to wait until Applicant section is done before solidifying what Collegs implementation we use.
--- src/app/modules/applicant/pages/applicant-professional/applicant-professional.component.ts
+++ src/app/modules/applicant/pages/applicant-professional/applicant-professional.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeDataService } from '../../../../services/prime-data.service';
+import { CollegeTypes } from '../../../../models/colleges.enum';
 
 @Component({
   selector: 'prime-applicant-professional',
@@ -7,14 +8,24 @@ import { PrimeDataService } from '../../../../services/prime-data.service';
   styleUrls: ['./applicant-professional.component.scss']
 })
 export class ApplicantProfessionalComponent implements OnInit {
+  public collegeTypesSelector: CollegeTypes;
 
   constructor(private dataService: PrimeDataService) { }
 
   ngOnInit() {
+    this.collegeTypesSelector = CollegeTypes.CPBC;
   }
 
   get applicant() {
     return this.dataService.user;
   }
 
+  // Make enum accessible to template
+  get CollegeTypes() {
+    return Object.keys(CollegeTypes);
+  }
+
+  // collegeTypes() {
+  //   let selection: CollegeTypes = this.collegeTypesSelector;
+  // }
 }
