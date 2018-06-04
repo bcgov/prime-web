3746ddff2f168e066b844470cd42a09e4a6cc5cf
denis-klein <denis.klein@maximusbc.ca>
2018-06-04 15:56:08 -0700

    Before local git

--- src/app/models/colleges.enum.ts
+++ src/app/models/colleges.enum.ts
@@ -3,18 +3,59 @@
  * Professional Information college choices
  */
 export enum CollegeTypes {
-  /**College of Pharmacists Of BC (CPBC)*/
-  CPBC = 'P1',
-  /**College of Registered Nurses of BC (CRNBC)*/
-  CRNBC = '96',
-  /**College of Physicians and Surgeons of BC (CPSBC)*/
-  CPSBC = '91'
+  CPBC  = 'P1' /**College of Pharmacists Of BC (CPBC)*/,
+  CRNBC = '96' /**College of Registered Nurses of BC (CRNBC)*/,
+  CPSBC = '91' /**College of Physicians and Surgeons of BC (CPSBC)*/
 }
 
-
+/**
+ * CPBC related license classes
+ */
 export enum LicenseClassCPTypes {
   FULL    = 'Full Pharmacist',
   LIMITED = 'Limited Pharmacist'
+}
+
+/**
+ * CRNBC related license classes
+ */
+export enum LicenseClassCRNTypes {
+  REGISTERED = 'Registered Nurse',
+  TEMP       = 'Temp. Registered Nurse'
+}
+
+/**
+ * CPSBC related license classes
+ */
+export enum LicenseClassCPSTypes {
+  GENERAL   = 'Full - General',
+  SPECIALTY = 'Full - Specialty'
+}
+
+/**
+ * CRNBC related Advanced Practice Certification
+ */
+export enum AdvancedPracticeCertificationTypes {
+  REMOTE       = 'Remote Practice',
+  REPRODUCTIVE = 'Reproductive Care',
+  STI          = 'Sexually Transmitted Infection',
+  NONE         = 'None'
+}
+
+/**
+ * Entries related to Working on-behalf
+ */
+export enum JobTitleTypes {
+  MEDICA = 'Medica Office Assistant',
+  OTHER  = 'Other'
+}
+
+/**
+ * Entries related to Working on-behalf
+ */
+export enum MaxLengthTypes {
+  LICENCE_NUMBER          = 12,
+  DEVICE_PROVIDER_NUMBER  = 12
 }
 
 
--- src/app/modules/applicant/pages/applicant-professional/applicant-professional.component.ts
+++ src/app/modules/applicant/pages/applicant-professional/applicant-professional.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PrimeDataService } from '../../../../services/prime-data.service';
 import { CollegeTypes } from '../../../../models/colleges.enum';
 import { LicenseClassCPTypes } from '../../../../models/colleges.enum';
+import { LicenseClassCRNTypes } from '../../../../models/colleges.enum';
+import { LicenseClassCPSTypes } from '../../../../models/colleges.enum';
+import { AdvancedPracticeCertificationTypes } from '../../../../models/colleges.enum';
+import { JobTitleTypes } from '../../../../models/colleges.enum';
+import { MaxLengthTypes } from '../../../../models/colleges.enum';
 
 @Component({
   selector: 'prime-applicant-professional',
@@ -9,8 +14,12 @@ import { LicenseClassCPTypes } from '../../../../models/colleges.enum';
   styleUrls: ['./applicant-professional.component.scss']
 })
 export class ApplicantProfessionalComponent implements OnInit {
-  public collegeTypesSelector        = 'pleaseSelect';
-  public licenseClassCPTypesSelector = 'pleaseSelect';
+  public collegeTypesSelector                       = 'pleaseSelect';
+  public licenseClassCPTypesSelector                = 'pleaseSelect';
+  public licenseClassCRNTypesSelector               = 'pleaseSelect';
+  public licenseClassCPSTypesSelector               = 'pleaseSelect';
+  public advancedPracticeCertificationTypesSelector = 'pleaseSelect';
+  public jobTitleTypesSelector                      = 'pleaseSelect';
 
   constructor(private dataService: PrimeDataService) { }
 
@@ -31,6 +40,31 @@ export class ApplicantProfessionalComponent implements OnInit {
     return Object.keys(LicenseClassCPTypes);
   }
 
+  // Make enum accessible to template
+  get LicenseClassCRNTypes() {
+    return Object.keys(LicenseClassCRNTypes);
+  }
+
+  // Make enum accessible to template
+  get LicenseClassCPSTypes() {
+    return Object.keys(LicenseClassCPSTypes);
+  }
+
+  // Make enum accessible to template
+  get AdvancedPracticeCertificationTypes() {
+    return Object.keys(AdvancedPracticeCertificationTypes);
+  }
+
+  // Make enum accessible to template
+  get JobTitleTypes() {
+    return Object.keys(JobTitleTypes);
+  }
+
+  // Make enum accessible to template
+  get MaxLengthTypes() {
+    return Object.keys(MaxLengthTypes);
+  }
+
   collegeValue() {
     const selection = this.collegeTypesSelector;
     return CollegeTypes[selection] ? CollegeTypes[selection] : '';
@@ -40,4 +74,23 @@ export class ApplicantProfessionalComponent implements OnInit {
     return LicenseClassCPTypes[selection];
   }
 
+  licenseClassCRNValue(selection) {
+    return LicenseClassCRNTypes[selection];
+  }
+
+  licenseClassCPSValue(selection) {
+    return LicenseClassCPSTypes[selection];
+  }
+
+  advancedPracticeCertificationValue(selection) {
+    return AdvancedPracticeCertificationTypes[selection];
+  }
+
+  jobTitleValue(selection) {
+    return JobTitleTypes[selection];
+  }
+
+  maxLengthValue(selection) {
+    return MaxLengthTypes[selection];
+  }
 }
