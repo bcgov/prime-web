d5fffe70eaea5bf2cf8d758f07a2f1f2376120d1
Adam Coard <acoard@advantcomp.com>
2018-06-13 07:47:52 -0700

    Angular 6 - Update references to HttpClietnModule / HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyDateRangePickerModule } from 'mydaterangepicker';
