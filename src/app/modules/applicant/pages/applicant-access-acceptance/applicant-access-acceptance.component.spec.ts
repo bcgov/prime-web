8cada17d7e7678903b898f123b31ecda5d2552b2
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2018-06-14 11:52:23 -0400

    Resolve merge conflict

--- src/app/modules/applicant/pages/applicant-access-acceptance/applicant-access-acceptance.component.spec.ts
+++ src/app/modules/applicant/pages/applicant-access-acceptance/applicant-access-acceptance.component.spec.ts
@@ -3,19 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApplicantAccessAcceptanceComponent } from './applicant-access-acceptance.component';
 import {ApplicantBreadcrumbsComponent} from '../../components/applicant-breadcrumbs/applicant-breadcrumbs.component';
 import {RouterTestingModule} from '@angular/router/testing';
-<<<<<<< HEAD
-
-import { FormsModule } from '@angular/forms';
-import {PrimeDataService} from '../../../../services/prime-data.service';
-
-=======
 import {FormsModule} from '@angular/forms';
 import {PrimeDataService} from '../../../../services/prime-data.service';
 import { CoreModule } from '../../../core/core.module';
 import { AlertModule } from 'ngx-bootstrap';
 import { APP_BASE_HREF } from '@angular/common';
 import { ApplicantDataService } from '../../../../services/applicant-data.service';
->>>>>>> master
 
 describe('ApplicantAccessAcceptanceComponent', () => {
   let component: ApplicantAccessAcceptanceComponent;
