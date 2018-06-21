09be6fd043131048a53d133d4177230c10c72773
Adam Coard <acoard@advantcomp.com>
2018-06-21 09:22:28 -0700

    All tests passing

--- src/app/modules/applicant/components/appl-enrollment-row/appl-enrollment-row.component.spec.ts
+++ src/app/modules/applicant/components/appl-enrollment-row/appl-enrollment-row.component.spec.ts
@@ -4,14 +4,15 @@ import { ApplEnrollmentRowComponent } from './appl-enrollment-row.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '../../../core/core.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppEnrollmentProgressRowComponent } from '../app-enrollment-progress-row/app-enrollment-progress-row.component';
 
-xdescribe('ApplEnrollmentRowComponent', () => {
+describe('ApplEnrollmentRowComponent', () => {
   let component: ApplEnrollmentRowComponent;
   let fixture: ComponentFixture<ApplEnrollmentRowComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ApplEnrollmentRowComponent ],
+      declarations: [ ApplEnrollmentRowComponent, AppEnrollmentProgressRowComponent ],
       imports: [NoopAnimationsModule, CoreModule, RouterTestingModule]
     })
     .compileComponents();
