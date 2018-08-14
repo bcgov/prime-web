b0812c71cba6e5834e386572699ffd9142b8105a
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2018-08-14 14:59:20 -0400

    Bread crumb navigates to next page when press continue

--- src/app/modules/registration/components/registration-container/registration-container.component.ts
+++ src/app/modules/registration/components/registration-container/registration-container.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {pageRoutes} from '../../registration-page-routing.module';
 import {WizardProgressItem} from '../../../core/components/wizard-progress-bar/wizard-progress-bar.component';
+import {Router} from '@angular/router';
+import {environment} from '../../../../../environments/environment';
 
 @Component({
   selector: 'prime-registration-container',
@@ -9,7 +11,9 @@ import {WizardProgressItem} from '../../../core/components/wizard-progress-bar/w
 })
 export class RegistrationContainerComponent implements OnInit {
 
-  constructor() { }
+  public allowLinks: boolean = !environment.production ? true : false;
+
+  constructor( private router: Router ) { }
 
   ngOnInit() {
     this.progressSteps = pageRoutes.map(x => {
@@ -30,5 +34,22 @@ export class RegistrationContainerComponent implements OnInit {
    */
   convertRouteToTitle(routePath: string): string{
     return routePath.split('-').map(x => x[0].toUpperCase() + x.slice(1)).join(' ');
+  }
+
+  /**
+   * Navigates through the pages
+   */
+  continue() {
+    // Find current index of URL
+    let idx = this.progressSteps.findIndex( x => {
+      return this.router.url.endsWith( x.route ); } );
+
+    // Case were route is blank
+    if ( -1 === idx ) { idx = 0; }
+
+    if ( this.progressSteps.length > idx + 1 ) {
+      // Navigate next page
+      this.router.navigate(['/register/' + this.progressSteps[idx + 1].route] );
+    }
   }
 }
