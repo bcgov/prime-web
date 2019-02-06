14604b7161eed242abe104b1f9ad0eaec842de31
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-02-06 17:48:21 -0500

    Populated shared-core folder with items to be moved to common lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/** These imports will change when moved to moh-common-styles project */
+import { FormActionBarComponent } from './shared-core/components/form-action-bar/form-action-bar.component';
+import { PageFrameworkComponent } from './shared-core/components/page-framework/page-framework.component';
+import { PasswordComponent } from './shared-core/components/password/password.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageFrameworkComponent,
+    FormActionBarComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
