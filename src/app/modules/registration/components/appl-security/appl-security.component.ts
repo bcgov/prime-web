733680d1b61a2ceacda61ee1f75ad61d12767c5c
cyril bravo <cyril.bravo@vs-dev015.maximusbchealth.local>
2019-03-05 18:52:10 -0500

    Modify security page

--- src/app/modules/registration/components/appl-security/appl-security.component.ts
+++ src/app/modules/registration/components/appl-security/appl-security.component.ts
@@ -3,6 +3,7 @@ import { PrimeDataService } from '../../../../services/prime-data.service';
 import { Registrant } from '../../models/registrant.model';
 import { CacheService } from '../../../../services/cache.service';
 import { ControlContainer, NgForm } from '@angular/forms';
+import { PrimeConstants } from '../../../../models/prime-constants';
 
 @Component({
   selector: 'prime-appl-security',
@@ -11,7 +12,7 @@ import { ControlContainer, NgForm } from '@angular/forms';
 })
 export class ApplSecurityComponent implements OnInit {
 
-  phoneNumber = "1-555-555-5555"
+  public useMobile;
 
   constructor( private primeDataService: PrimeDataService,
                private cache: CacheService  ) {
@@ -25,4 +26,13 @@ export class ApplSecurityComponent implements OnInit {
     return this.primeDataService.registrant;
   }
 
+  isCanada(): boolean {
+    if ( !this.registrant.address ) {
+      return true; // Default to Canada
+    } 
+    else if ( this.registrant.identityIsMailingAddress ) {
+      return (this.registrant.address.country === PrimeConstants.CANADA);
+    }
+    return (this.registrant.mailAddress.country === PrimeConstants.CANADA);
+  }
 }
