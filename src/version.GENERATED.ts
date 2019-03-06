d6d5efba3675eaaa45d09f93eed23fe61e5e06d8
Adam Coard <arcoard+github@gmail.com>
2019-03-06 13:58:14 -0500

    Ran moh-common-schematics:version-js console.log version for EOW build

---
+++ src/version.GENERATED.ts
@@ -0,0 +1,9 @@
+// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
+export const gitCommit = '27f92a8';
+export const buildTime = '3/6/2019 at 10:56:27 AM';
+/** App version retrieved from package.json. */
+export const projectVersion = '0.0.0';
+/** Human readable message  */
+export const message = 'Canonical Version: 27f92a8 - 3/6/2019 at 10:56:27 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/27f92a8';
+/** If true, other values should be present. */
+export const success = true;
