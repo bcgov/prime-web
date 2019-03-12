8816672ebfefc345ff3458107310f5777741179f
Adam Coard <arcoard+github@gmail.com>
2019-03-12 18:54:21 -0400

    Replaced application files with build of prototype-v2-old.

--- src/version.GENERATED.ts
+++
@@ -1,9 +0,0 @@
-// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '813a69d';
-export const buildTime = '3/11/2019 at 2:07:31 PM';
-/** App version retrieved from package.json. */
-export const projectVersion = '0.0.0';
-/** Human readable message  */
-export const message = 'Canonical Version: 813a69d - 3/11/2019 at 2:07:31 PM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/813a69d';
-/** If true, other values should be present. */
-export const success = true;
