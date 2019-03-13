971426ff3f514f261cad439a4eddf0344f08b142
Adam Coard <arcoard+github@gmail.com>
2019-03-13 14:11:14 -0400

    Auto-generate versionjs when running npm run build:reg

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '856b16c';
-export const buildTime = '3/13/2019 at 11:00:45 AM';
+export const gitCommit = '5c9b8b2';
+export const buildTime = '3/13/2019 at 11:10:47 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 856b16c - 3/13/2019 at 11:00:45 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/856b16c';
+export const message = 'Canonical Version: 5c9b8b2 - 3/13/2019 at 11:10:47 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/5c9b8b2';
 /** If true, other values should be present. */
 export const success = true;
