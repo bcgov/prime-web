856b16c58c301c1b35ab664c58a705ad0d818ff2
Adam Coard <arcoard+github@gmail.com>
2019-03-13 13:39:41 -0400

    Serve is working but builds fail with a core-js error.

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '813a69d';
-export const buildTime = '3/11/2019 at 2:07:31 PM';
+export const gitCommit = 'fc5111d';
+export const buildTime = '3/13/2019 at 10:37:53 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 813a69d - 3/11/2019 at 2:07:31 PM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/813a69d';
+export const message = 'Canonical Version: fc5111d - 3/13/2019 at 10:37:53 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/fc5111d';
 /** If true, other values should be present. */
 export const success = true;
