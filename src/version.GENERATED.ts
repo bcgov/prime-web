5c9b8b2d1f64edfb7808db2a8490673d5bdbc4c1
Adam Coard <arcoard+github@gmail.com>
2019-03-13 14:03:03 -0400

    Commit after compiling success - just had to update core-js minor version.

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'fc5111d';
-export const buildTime = '3/13/2019 at 10:37:53 AM';
+export const gitCommit = '856b16c';
+export const buildTime = '3/13/2019 at 11:00:45 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: fc5111d - 3/13/2019 at 10:37:53 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/fc5111d';
+export const message = 'Canonical Version: 856b16c - 3/13/2019 at 11:00:45 AM (v0.0.0) \nhttps://github.com/bcgov/prime-web/commits/856b16c';
 /** If true, other values should be present. */
 export const success = true;
