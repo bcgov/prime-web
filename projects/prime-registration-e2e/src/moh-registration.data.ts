a4f28eff5c26b2533ebf3fb9785b662d1c12236f
Adam Coard <arcoard+github@gmail.com>
2019-03-13 14:54:27 -0400

    prime-registration-e2e tests copied over, but failing on dropdowns

---
+++ projects/prime-registration-e2e/src/moh-registration.data.ts
@@ -0,0 +1,36 @@
+import * as faker from 'faker';
+
+export class FakeDataMohReg {
+
+    profileInfo(): ProfilePageTest {
+        return {
+            firstName: faker.name.firstName(),
+            middleName: Math.random() > 0.5 ? faker.name.firstName() : undefined,
+            lastName: faker.name.lastName(),
+            birthDate: faker.date.past(),
+            country: faker.address.country(),
+            address: faker.address.streetAddress(),
+            city: faker.address.city(),
+            postal: faker.address.zipCode('?#? #?#')
+        };
+    }
+}
+
+// TODO - Refactor and improve this so we can create interfaces / types automatically at compiletime based off the return type.
+// Create interfaces automatically based off the method return types.
+// const data = new FakeDataMohReg();
+// export type TestProfileInfo = typeof data.profileInfo();
+
+export interface ProfilePageTest {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  birthDate: Date;
+  country: string;
+  address: string;
+  city: string;
+  postal: string;
+
+  /** Currently province is not auto-generated and is manually added during tests. */
+  province?: string;
+}
