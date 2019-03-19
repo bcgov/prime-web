63d78346a3b2de125b19537c1bd5e072223061c9
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-03-19 16:10:03 -0700

    imported profile page as per prime 334 but need to create a base component for the profile component to be extended by the existing profile component

--- projects/prime-enrollment/src/app/shared/shared.module.ts
+++ projects/prime-enrollment/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCoreModule } from 'moh-common-lib';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegistrationModule } from '../../../../../projects/prime-registration/src/app/modules/registration/registration.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, SharedCoreModule],
+  imports: [CommonModule, SharedCoreModule, RegistrationModule],
   exports: [SharedCoreModule, HomePageComponent]
 })
 export class SharedModule {}
