1e18dc821fab4be3d940536a9c3adcbf5bfa3a69
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-03-20 14:02:25 -0700

    updates to module import for prime-334.

--- projects/prime-enrollment/src/app/shared/shared.module.ts
+++ projects/prime-enrollment/src/app/shared/shared.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCoreModule } from 'moh-common-lib';
-import { RegistrationModule } from '../../../../../projects/prime-registration/src/app/modules/registration/registration.module';
+import { ProfileComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/profile/profile.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { EnrollmentProfileComponent } from './components/enrollment-profile/enrollment-profile.component';
 
 @NgModule({
-  declarations: [HomePageComponent, EnrollmentProfileComponent],
-  imports: [CommonModule, SharedCoreModule, RegistrationModule],
-  exports: [SharedCoreModule, HomePageComponent, EnrollmentProfileComponent]
+  declarations: [
+    HomePageComponent,
+    EnrollmentProfileComponent,
+    ProfileComponent
+  ],
+  imports: [CommonModule, SharedCoreModule],
+  exports: [SharedCoreModule, HomePageComponent]
 })
 export class SharedModule {}
