91e51e106bbfcb40d97d347fb4b6591245b1cc24
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-03-20 14:11:44 -0700

    fixe da couple of template errors for prime-334

--- projects/prime-enrollment/src/app/shared/shared.module.ts
+++ projects/prime-enrollment/src/app/shared/shared.module.ts
@@ -4,14 +4,20 @@ import { SharedCoreModule } from 'moh-common-lib';
 import { ProfileComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/profile/profile.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { EnrollmentProfileComponent } from './components/enrollment-profile/enrollment-profile.component';
+import { NameComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/name/name.component';
+import { AddressComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/address/address.component';
+import { PageSectionsComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/page-sections/page-sections.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     EnrollmentProfileComponent,
-    ProfileComponent
+    ProfileComponent,
+    NameComponent,
+    AddressComponent,
+    PageSectionsComponent
   ],
   imports: [CommonModule, SharedCoreModule],
-  exports: [SharedCoreModule, HomePageComponent]
+  exports: [SharedCoreModule, HomePageComponent, ProfileComponent]
 })
 export class SharedModule {}
