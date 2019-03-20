3a193e5c6d2f5a0efaa2f8e7178e9dd336c758b3
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-03-20 13:14:09 -0700

    this is a WIP commit as I need to switch over to the dev/dummy-profile branch. This is for Prime-334

--- projects/prime-enrollment/src/app/app-routing.module.ts
+++ projects/prime-enrollment/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './shared/home-page/home-page.component';
+import { HomePageComponent } from './shared/components/home-page/home-page.component';
 
 const routes: Routes = [
   {
--- projects/prime-enrollment/src/app/shared/shared.module.ts
+++ projects/prime-enrollment/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCoreModule } from 'moh-common-lib';
-import { HomePageComponent } from './home-page/home-page.component';
 import { RegistrationModule } from '../../../../../projects/prime-registration/src/app/modules/registration/registration.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { EnrollmentProfileComponent } from './components/enrollment-profile/enrollment-profile.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, EnrollmentProfileComponent],
   imports: [CommonModule, SharedCoreModule, RegistrationModule],
-  exports: [SharedCoreModule, HomePageComponent]
+  exports: [SharedCoreModule, HomePageComponent, EnrollmentProfileComponent]
 })
 export class SharedModule {}
