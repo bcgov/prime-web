d6ce1183cd637b5eaba65e9f51bfc8b5b42f1e20
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-03-27 12:59:48 -0400

    Fixed display of province on when refresh browser, remove import that did not exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomePageComponent,
-    data: { title: 'Home'}
-  },
-  {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
