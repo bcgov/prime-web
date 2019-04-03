3576ea71e91dc9ad71449ca73d38d0ffebdfb833
Adam Coard <arcoard+github@gmail.com>
2019-04-03 12:12:35 -0400

    Update registration/environment.prod.ts for cache url

--- projects/prime-registration/src/environments/environment.prod.ts
+++ projects/prime-registration/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMockBackend: false,
-  cacheAPIUrl: '/cache/api/reg/rest/getCache',
+  cacheAPIUrl: '/api/reg/rest/getCache',
   baseAPIUrl: '/api/rest/'
 };
