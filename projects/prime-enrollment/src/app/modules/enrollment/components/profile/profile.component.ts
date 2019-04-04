ed16077f79e6b10009c94ae334ddc06586c04384
Adam Coard <arcoard+github@gmail.com>
2019-04-04 14:30:39 -0400

    Re-use Profile comp in Enrolment w/ CacheService; add proxy for cache

--- projects/prime-enrollment/src/app/modules/enrollment/components/profile/profile.component.ts
+++ projects/prime-enrollment/src/app/modules/enrollment/components/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { EnrollmentStateService } from '../../services/enrollment-state.service';
 import { countryList, provinceList } from '../../data/country';
+import { CacheService } from '@prime-core/services/cache.service';
+
+
 const country = countryList;
 const province = provinceList;
 @Component({
@@ -9,9 +12,12 @@ const province = provinceList;
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  constructor(public enrollStateSvc: EnrollmentStateService) {}
-  countries = country;
-  provinces = province;
+  // TODO - replace CacheService with EnrolmentCacheService when created.
+  constructor(public enrollStateSvc: EnrollmentStateService, private cacheService: CacheService) {
+  }
+
+  countries = this.cacheService.$countryList;
+  provinces = this.cacheService.$provinceList;
 
   ngOnInit() {}
 }
