26784f1763c457cbcaca91e8861d3152ebf983cd
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-04 09:44:10 -0700

    finished adding review page details

--- projects/prime-enrollment/src/app/shared/shared.module.ts
+++ projects/prime-enrollment/src/app/shared/shared.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCoreModule } from 'moh-common-lib';
-import { ProfileComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/profile/profile.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { EnrollmentProfileComponent } from './components/enrollment-profile/enrollment-profile.component';
-import { NameComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/name/name.component';
-import { AddressComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/address/address.component';
-import { PageSectionsComponent } from '../../../../../projects/prime-registration/src/app/modules/registration/components/page-sections/page-sections.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { TypeaheadModule, BsDropdownModule } from 'ngx-bootstrap';
@@ -33,15 +29,14 @@ import { SelfDeclarationBlockComponent } from './components/self-declaration-blo
 import { SelfDeclarationQuestionBlockComponent } from './components/self-declaration-question-block/self-declaration-question-block.component';
 import { ProfessionalInformationBlockComponent } from './components/professional-information-block/professional-information-block.component';
 import { PharmanetBlockComponent } from './components/pharmanet-block/pharmanet-block.component';
+import { ProfileComponent } from '@prime-enrollment/modules/enrollment/components/profile/profile.component';
+import { PrimeSharedModule } from '../../../../../src/app/prime-shared/prime-shared.module';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     EnrollmentProfileComponent,
-    ProfileComponent,
-    NameComponent,
-    AddressComponent,
-    PageSectionsComponent,
+    // ProfileComponent,
     PhoneInputComponent,
     SharedProfileComponent,
     LicenseComponent,
@@ -71,7 +66,8 @@ import { PharmanetBlockComponent } from './components/pharmanet-block/pharmanet-
     NgxIntlTelInputModule,
     InternationalPhoneNumberModule,
     MatDialogModule,
-    MatCardModule
+    MatCardModule,
+    PrimeSharedModule
   ],
   exports: [
     FormsModule,
@@ -88,7 +84,6 @@ import { PharmanetBlockComponent } from './components/pharmanet-block/pharmanet-
     DeviceProviderComponent,
     YesNoComponent,
     DetailsComponent,
-    PageSectionsComponent,
     ActionBlockComponent,
     MatCardModule,
     ListItemComponent,
@@ -99,7 +94,8 @@ import { PharmanetBlockComponent } from './components/pharmanet-block/pharmanet-
     SelfDeclarationBlockComponent,
     SelfDeclarationQuestionBlockComponent,
     ProfessionalInformationBlockComponent,
-    PharmanetBlockComponent
+    PharmanetBlockComponent,
+    PrimeSharedModule
   ]
 })
 export class SharedModule {}
