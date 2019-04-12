b665dc7c425f5966e5d9c43afb2096b5c050d501
Adam Coard <arcoard+github@gmail.com>
2019-04-12 16:31:01 -0400

    included updated version file

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '034effc';
-export const buildTime = '4/12/2019 at 1:22:22 PM';
+export const gitCommit = '5d19ca5';
+export const buildTime = '4/12/2019 at 1:25:01 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 034effc - 4/12/2019 at 1:22:22 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/034effc';
+export const message = 'Canonical Version: 5d19ca5 - 4/12/2019 at 1:25:01 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/5d19ca5';
 /** If true, other values should be present. */
 export const success = true;
