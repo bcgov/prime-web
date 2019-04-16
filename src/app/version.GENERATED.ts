5008556af6f732d16a40e4c3de228048f2ffee6e
Adam Coard <arcoard+github@gmail.com>
2019-04-16 11:17:19 -0400

    Implement datepicker form control with moh-common-lib@0.027

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5a59e81';
-export const buildTime = '4/12/2019 at 2:12:45 PM';
+export const gitCommit = '2da6926';
+export const buildTime = '4/12/2019 at 3:00:03 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 5a59e81 - 4/12/2019 at 2:12:45 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/5a59e81';
+export const message = 'Canonical Version: 2da6926 - 4/12/2019 at 3:00:03 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/2da6926';
 /** If true, other values should be present. */
 export const success = true;
