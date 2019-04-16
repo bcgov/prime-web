4cf88683b85d4837577b09fd11097c97f30eb86f
Adam Coard <arcoard+github@gmail.com>
2019-04-16 11:35:32 -0400

    Fix: Change SearchOrgModalComp StateSVC to public as it's in template.

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '2da6926';
-export const buildTime = '4/12/2019 at 3:00:03 PM';
+export const gitCommit = '69d4af3';
+export const buildTime = '4/16/2019 at 8:34:30 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 2da6926 - 4/12/2019 at 3:00:03 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/2da6926';
+export const message = 'Canonical Version: 69d4af3 - 4/16/2019 at 8:34:30 AM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/69d4af3';
 /** If true, other values should be present. */
 export const success = true;
