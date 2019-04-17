dfd368e328f23b6e6ef50b45b811c45f3d77346c
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-17 11:24:09 -0700

    updated the modal to now use the bootstrap modal

--- projects/prime-enrollment/src/app/modules/enrollment/components/pharmanet-access/pharmanet-access.component.ts
+++ projects/prime-enrollment/src/app/modules/enrollment/components/pharmanet-access/pharmanet-access.component.ts
@@ -2,15 +2,16 @@ import {
   Component,
   OnInit,
   ChangeDetectionStrategy,
-  OnDestroy
+  OnDestroy,
+  TemplateRef
 } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
 import { EnrollmentStateService } from '../../services/enrollment-state.service';
 import { SearchOrganizationModalComponent } from '../search-organization-modal/search-organization-modal.component';
-import { MatDialog } from '@angular/material/dialog';
 import { EnrollmentDataService } from '../../services/enrollment-data.service';
 import { Observable, of, BehaviorSubject, Subscription } from 'rxjs';
 import { INgxMyDpOptions } from 'ngx-mydatepicker';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 const headers = ['Organization Name', 'Type', 'City'];
 @Component({
   selector: 'app-pharmanet-access',
@@ -21,17 +22,18 @@ const headers = ['Organization Name', 'Type', 'City'];
 export class PharmanetAccessComponent implements OnInit, OnDestroy {
   fa$: BehaviorSubject<FormGroup[]> = new BehaviorSubject(null);
   results = false;
+  modalRef: BsModalRef;
   dateOptions: INgxMyDpOptions = {
     // other options...
     dateFormat: 'dd.mm.yyyy',
     openSelectorTopOfInput: true,
     showSelectorArrow: false
   };
-  sub: Subscription;
+  // sub: Subscription;
 
   constructor(
     private stateSvc: EnrollmentStateService,
-    public dialog: MatDialog
+    private modalSvc: BsModalService
   ) {}
 
   ngOnInit() {
@@ -40,28 +42,41 @@ export class PharmanetAccessComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if (this.sub) this.sub.unsubscribe();
+    // if (this.sub) this.sub.unsubscribe();
   }
 
-  addOrganization() {
-    return this.openModal();
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalSvc.show(template, {
+      backdrop: true,
+      ignoreBackdropClick: false,
+      class: 'modal-md'
+    });
+    // const dialog = this.dialog;
+    // const ref = dialog.open(SearchOrganizationModalComponent, {
+    //   panelClass: 'test',
+    //   disableClose: true,
+    //   position: { top: '25px' },
+    //   minWidth: '50vw'
+    // });
+    // this.sub = ref.afterClosed().subscribe(obs => {
+    //   if (!obs) return;
+    //   const arr = this.stateSvc.organizationForm;
+    //   this.stateSvc.organizationForm = obs;
+    //   this.fa$.next(this.stateSvc.organizationForm);
+    //   this.results = true;
+    // });
   }
 
-  openModal() {
-    const dialog = this.dialog;
-    const ref = dialog.open(SearchOrganizationModalComponent, {
-      panelClass: 'test',
-      disableClose: true,
-      position: { top: '25px' },
-      minWidth: '50vw'
-    });
-    this.sub = ref.afterClosed().subscribe(obs => {
-      if (!obs) return;
-      const arr = this.stateSvc.organizationForm;
-      this.stateSvc.organizationForm = obs;
-      this.fa$.next(this.stateSvc.organizationForm);
-      this.results = true;
-    });
+  modalResult(evt: any) {
+    const arr = this.stateSvc.organizationForm;
+    this.stateSvc.organizationForm = evt;
+    this.fa$.next(this.stateSvc.organizationForm);
+    this.results = true;
+    // this.stateSvc.organizaitonForm
+  }
+
+  modalSubmit() {
+    return this.modalRef.hide();
   }
 
   remove(i: number) {
