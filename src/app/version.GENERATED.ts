bdfc5b3f2300fe2306ea75f4ddbcca4e9d14e6db
Adam Coard <arcoard+github@gmail.com>
2019-04-17 14:51:26 -0400

    chore: committing auto-gen version.

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5455403';
-export const buildTime = '4/16/2019 at 3:01:41 PM';
+export const gitCommit = '4c8b349';
+export const buildTime = '4/17/2019 at 11:51:06 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 5455403 - 4/16/2019 at 3:01:41 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/5455403';
+export const message = 'Canonical Version: 4c8b349 - 4/17/2019 at 11:51:06 AM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/4c8b349';
 /** If true, other values should be present. */
 export const success = true;
