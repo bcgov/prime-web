e51ec30f60bda4be7471df9ffae92f9a5bfd564a
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-04-24 11:50:46 -0400

    Library compiled with confirmation and api-base classes

---
+++ projects/prime-core/src/lib/components/confirmation/confirmation.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ApiStatusCodes } from '../../models/api-base.model';
+
+@Component({
+  selector: 'lib-prime-confirmation',
+  templateUrl: './confirmation.component.html',
+  styleUrls: ['./confirmation.component.scss']
+})
+export class ConfirmationComponent implements OnInit {
+  @Input() displayIcon: ApiStatusCodes = ApiStatusCodes.SUCCESS;
+  @Input() hasQrCode: boolean = false;
+  @Input() btnLabel: string = 'Login';
+
+  @Output() btnClick: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  // Status codes
+  get successCode() {
+    return ApiStatusCodes.SUCCESS;
+  }
+
+  get errorCode() {
+    return ApiStatusCodes.ERROR;
+  }
+
+  get warningCode() {
+    return ApiStatusCodes.WARNING;
+  }
+
+  onClick($event) {
+    this.btnClick.emit($event);
+    $event.stopPropagation();
+    return false;
+  }
+}
--- projects/prime-core/src/public-api.ts
+++ projects/prime-core/src/public-api.ts
@@ -9,3 +9,4 @@ export * from './lib/prime-core.module';
 // Models
 export * from './lib/models/prime-constants';
 export { PrimePerson } from './lib/models/prime-person.model';
+export * from './lib/models/api-base.model';
