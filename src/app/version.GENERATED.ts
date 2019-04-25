7cf978ae2633b88941e3476e474b1998b6af95ba
Adam Coard <arcoard+github@gmail.com>
2019-04-25 14:42:00 -0400

    Bumping moh-common-lib to 0.0.30 to fix datepicker bug

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'd99ca4f';
-export const buildTime = '4/17/2019 at 1:33:40 PM';
+export const gitCommit = '851e1473';
+export const buildTime = '4/25/2019 at 10:39:25 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: d99ca4f - 4/17/2019 at 1:33:40 PM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/d99ca4f';
+export const message = 'Canonical Version: 851e1473 - 4/25/2019 at 10:39:25 AM (v0.0.1) \nhttps://github.com/bcgov/prime-web/commits/851e1473';
 /** If true, other values should be present. */
 export const success = true;
