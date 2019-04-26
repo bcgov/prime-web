b9df443d87af6434c37ec947025f45361c3a35ba
Adam Coard <arcoard+github@gmail.com>
2019-04-26 12:47:58 -0400

    bump version #

--- src/app/version.GENERATED.ts
+++ src/app/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '9a2647ad';
-export const buildTime = '4/25/2019 at 2:49:03 PM';
+export const gitCommit = 'af3c186c';
+export const buildTime = '4/25/2019 at 3:30:29 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 9a2647ad - 4/25/2019 at 2:49:03 PM (v0.0.2) \nhttps://github.com/bcgov/prime-web/commits/9a2647ad';
+export const message = 'Canonical Version: af3c186c - 4/25/2019 at 3:30:29 PM (v0.0.2) \nhttps://github.com/bcgov/prime-web/commits/af3c186c';
 /** If true, other values should be present. */
 export const success = true;
