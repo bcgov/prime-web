9aca5761edb64ac225f1ea312f985b9f17779787
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-26 08:51:08 -0700

    fixed stepper unit tests

--- projects/prime-enrollment/src/app/core/guards/stepper.guard.spec.ts
+++ projects/prime-enrollment/src/app/core/guards/stepper.guard.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { StepperGuard } from './stepper.guard';
+import { EnrollmentStateService } from '@prime-enrollment/modules/enrollment/services/enrollment-state.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CacheService } from '@prime-core/services/cache.service';
+import { CacheApiService } from '@prime-core/services/cache-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('StepperGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [StepperGuard]
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [
+        CacheService,
+        StepperGuard,
+        EnrollmentStateService,
+        CacheApiService
+      ]
     });
   });
 
