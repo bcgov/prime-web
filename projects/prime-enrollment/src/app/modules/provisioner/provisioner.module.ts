d35606ae4a3ce2efdec6abeb37908b36860154bd
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-29 13:58:42 -0700

    moved prime constants for enrolment to data folder

---
+++ projects/prime-enrollment/src/app/modules/provisioner/provisioner.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProvisionerRoutingModule } from './provisioner-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProvisionerRoutingModule
+  ]
+})
+export class ProvisionerModule { }
