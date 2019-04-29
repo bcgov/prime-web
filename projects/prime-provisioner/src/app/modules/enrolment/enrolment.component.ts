543c9fdb2416a35604fd3060560ec92d83d42947
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-29 15:18:02 -0700

    corrected error in the port on the prime-provisioner project angular.json settings

--- projects/prime-provisioner/src/app/modules/enrolment/enrolment.component.ts
+++ projects/prime-provisioner/src/app/modules/enrolment/enrolment.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Container } from 'moh-common-lib/models';
+import { subRoutes } from '@prime-prov/core/models/sub-routes';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'prov-enrolment',
   template: `
-    <p>
-      enrolment works!
-    </p>
+    <common-page-framework layout="blank">
+      <h1>Application Enrolment</h1>
+    </common-page-framework>
+    <common-core-breadcrumb>
+      <common-wizard-progress-bar
+        center
+        [progressSteps]="progressSteps"
+      ></common-wizard-progress-bar>
+    </common-core-breadcrumb>
+
+    <router-outlet></router-outlet>
+    <common-form-action-bar
+      (btnClick)="advancePage()"
+      [defaultColor]="!stateSvc.submit"
+      [submitLabel]="submitLabel$ | async"
+      [isLoading]="loading"
+    ></common-form-action-bar>
   `,
   styleUrls: ['./enrolment.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EnrolmentComponent implements OnInit {
+export class EnrolmentComponent extends Container implements OnInit {
+  loading = false;
+  submitLabel$ = from('Continue');
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
+    this.setProgressSteps(subRoutes);
   }
 
+  ngOnInit() {}
+
+  advancePage() {
+    console.log('advance the page');
+  }
 }
