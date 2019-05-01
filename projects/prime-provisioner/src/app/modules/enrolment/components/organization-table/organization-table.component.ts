03431912e75e24e6e4ed35fd6dbda69e454deefb
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-05-01 16:44:00 -0700

    completed adding and styling the card with the headers on it

--- projects/prime-provisioner/src/app/modules/enrolment/components/organization-table/organization-table.component.ts
+++ projects/prime-provisioner/src/app/modules/enrolment/components/organization-table/organization-table.component.ts
@@ -10,7 +10,24 @@ import { IOrganization } from '@prime-prov/core/interfaces/i-organization';
   selector: 'prov-organization-table',
   template: `
     <!-- TODO: accordion -->
-    <prov-panel [label]="org.name"> abcd </prov-panel>
+    <prov-panel [label]="org.name">
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col-3">
+              <span>Site Info</span>
+            </div>
+            <div class="col"><span>Site Type</span></div>
+            <div class="col"><span>Vendor</span></div>
+            <div class="col"><span>Status</span></div>
+            <div class="col"><span>Group Name</span></div>
+          </div>
+        </div>
+        <div class="card-body">
+          abcdef
+        </div>
+      </div>
+    </prov-panel>
     <!-- TODO: list header - use panel -->
     <!-- TODO: list items: list? -->
   `,
