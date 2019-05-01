4f5d4310edce585da55137ad9565e68d1b8d0ada
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-05-01 10:07:04 -0700

    updated cache service to retrieve the country list

--- projects/prime-provisioner/src/app/app.module.ts
+++ projects/prime-provisioner/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EnrolmentModule } from './modules/enrolment/enrolment.module';
 import { CoreModule } from './core/core.module';
+import { BASE_URL } from 'prime-core';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, EnrolmentModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, EnrolmentModule],
+  providers: [
+    {
+      provide: BASE_URL,
+      useValue: environment.baseAPIUrl
+    }
+  ],
   bootstrap: [AppComponent],
-  exports: [CoreModule]
+  exports: []
 })
 export class AppModule {}
