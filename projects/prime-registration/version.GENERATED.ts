65e6fd2c32a832a14e98bdadf8fc652ea972e317
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-05-24 18:39:15 -0400

    Checkpoint - able to build library

--- projects/prime-registration/version.GENERATED.ts
+++ projects/prime-registration/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '793951c3';
-export const buildTime = '4/26/2019 at 3:28:24 PM';
+export const gitCommit = '94965820';
+export const buildTime = '5/22/2019 at 10:05:29 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 793951c3 - 4/26/2019 at 3:28:24 PM (v0.0.2) \nhttps://github.com/bcgov/prime-web/commits/793951c3';
+export const message = 'Canonical Version: 94965820 - 5/22/2019 at 10:05:29 AM (v0.0.2) \nhttps://github.com/bcgov/prime-web/commits/94965820';
 /** If true, other values should be present. */
 export const success = true;
