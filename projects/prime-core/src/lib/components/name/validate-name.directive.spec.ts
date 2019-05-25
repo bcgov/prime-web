bf418081bbf745907c686c00f1510eabf3d249a1
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-05-25 00:12:26 -0400

    New prime-core - name component removed uses shared, profile updated to use common name from shared lib

--- projects/prime-core/src/lib/components/name/validate-name.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateNameDirective } from './validate-name.directive';
-
-describe('ValidateNameDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateNameDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/prime-core/src/lib/prime-core.module.ts
+++ projects/prime-core/src/lib/prime-core.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { ProfileComponent } from './components/profile/profile.component';
-import { NameComponent } from './components/name/name.component';
 import { SharedCoreModule } from 'moh-common-lib';
 import { CommonModule } from '@angular/common';
 import { NgSelectModule } from '@ng-select/ng-select';
@@ -9,7 +8,6 @@ import { FormsModule } from '@angular/forms';
 import { TypeaheadModule } from 'ngx-bootstrap';
 import { PhoneNumberComponent } from './components/phone-number/phone-number.component';
 import { ConfirmationComponent } from './components/confirmation/confirmation.component';
-import { ValidateNameDirective } from './components/name/validate-name.directive';
 import { VerifyPreferNameDirective } from './components/profile/verify-prefer-name.directive';
 import { ProfileBlockComponent } from './components/profile-block/profile-block.component';
 import { ContactBlockComponent } from './components/contact-block/contact-block.component';
@@ -17,11 +15,9 @@ import { ContactBlockComponent } from './components/contact-block/contact-block.
 import { SelfDeclarationQuestionBlockComponent } from './components/self-declaration-question-block/self-declaration-question-block.component';
 
 const componentList = [
-  NameComponent,
   ProfileComponent,
   PhoneNumberComponent,
   ConfirmationComponent,
-  ValidateNameDirective,
   VerifyPreferNameDirective,
   ProfileBlockComponent,
   ContactBlockComponent,
